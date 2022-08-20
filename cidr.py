2b64e63d6fb9c2b48cc488d5a65fc6eb3f83d4e6
GuiCT <gtomiasi@gmail.com>
2022-08-20 19:40:46 -0300

    adicionando wildcard e número de hosts
    incluindo anotações de tipo
    
    formatando código segundo pep8

--- cidr.py
+++ cidr.py
@@ -1,4 +1,6 @@
 import re  # Regex
+from ctypes import c_uint32  # Unsigned int utilizado para garantir
+# o funcionamento do bitwise NOT
 
 # Pattern utilizado para verificar formato de IP
 pattern = re.compile(
@@ -11,7 +13,7 @@ CIDR_MAX = 30
 # Separa um ip no formato válido em octetos e CIDR
 
 
-def separate_ip(ip: str):
+def separate_ip(ip: str) -> tuple[list[int], int]:
     # Separando octetos
     octetos = ip.split(".")
     # Separando último octeto do CIDR
@@ -24,7 +26,7 @@ def separate_ip(ip: str):
 # Escreve um ip no formato válido a partir dos octetos
 
 
-def group_ip(octets):
+def group_ip(octets: list[int]) -> str:
     # [192, 168, 0, 1] -> '192.168.0.1'
     outstr = "{}.{}.{}.{}".format(
         octets[0],
@@ -37,7 +39,7 @@ def group_ip(octets):
 # Escreve ip como um inteiro de 32 bits
 
 
-def ip_as_binary(octets):
+def ip_as_binary(octets: list[int]) -> int:
     # [192, 168, 0, 1] -> 11000000101010000000000000000001
     asbinary = (
         (octets[0] << 24) +
@@ -48,7 +50,7 @@ def ip_as_binary(octets):
     return asbinary
 
 
-def ip_from_binary(ip):
+def ip_from_binary(ip: int) -> list[int]:
     # 11000000101010000000000000000001 -> [192, 168, 0, 1]
     octets = list([None] * 4)
     octets[0] = ip >> 24
@@ -80,50 +82,51 @@ def check_ip_address(inputstr: str) -> bool:
 # Realiza um prompt pedindo pelo endereço IP, checa se o formato é válido.
 
 
-def get_ip_address():
+def get_ip_address() -> str:
     inputstr = input('Insira um endereço IP (w.x.y.z/n): ')
     if check_ip_address(inputstr):
         return inputstr
 
 
-def calculate_from_ip(ip):
+def calculate_from_ip(ip: str) -> dict:
+    # Variável de retorno, dicionário contendo os valores calculados
+    ret = dict()
     # Separando octetos e CIDR
     octetos, cidr = separate_ip(ip)
     # Escrevendo ip como um inteiro de 32 bits
     ip_binario = ip_as_binary(octetos)
     # Escrevendo máscara de subredes como inteiro de 32 bits
-    mascara_sub_rede = '1' * cidr + '0' * (32 - cidr)
-    mascara_sub_rede = int(mascara_sub_rede, 2)
-    # Máscara de sub-rede invertida
-    # (O bitwise NOT calcula o complemento de 2 do número, ao invés
-    # de inverter bit a bit).
-    not_mascara = int('1' * (32 - cidr), 2)
+    mascara_sub_rede = '1' * cidr + '0' * (32 - cidr)  # Escreve como string
+    ret['mascara_sub_rede'] = int(mascara_sub_rede, 2)  # Realiza parse
+    ret['mascara_wildcard'] = c_uint32(
+        ~ret['mascara_sub_rede']).value  # Bitwise NOT na
+    # máscara de sub-rede retorna a máscara wildcard.
     # Endereço de rede
-    end_rede = ip_binario & mascara_sub_rede
+    ret['end_rede'] = ip_binario & ret['mascara_sub_rede']
     # End de broadcast
-    end_broadcast = ip_binario | not_mascara
+    ret['end_broadcast'] = ip_binario | ret['mascara_wildcard']
     # Inicio e fim do range
-    range_start = end_rede + 1
-    range_end = end_broadcast - 1
-    # Retornando cada um dos valores calculados
-    return (end_rede, end_broadcast, mascara_sub_rede, range_start, range_end)
+    ret['range'] = dict()
+    ret['range']['start'] = ret['end_rede'] + 1
+    ret['range']['end'] = ret['end_broadcast'] - 1
+    # Quantidade de hosts disponíveis
+    ret['qnt_hosts'] = 2**(32 - cidr) - 2
+    return ret
 
 
 while True:
     try:
         inputstr = get_ip_address()
-        (end_rede,
-         end_broadcast,
-         mascara_sub_rede,
-         range_start,
-         range_end) = calculate_from_ip(inputstr)
+        infos = calculate_from_ip(inputstr)
         print(
             f'''Valor inserido: {inputstr}
 ================================================================================
-Endereço de rede: {group_ip(ip_from_binary(end_rede))}
-Endereço de broadcast: {group_ip(ip_from_binary(end_broadcast))}
-Máscara de sub-rede: {group_ip(ip_from_binary(mascara_sub_rede))}
-Range: {group_ip(ip_from_binary(range_start))} - {group_ip(ip_from_binary(range_end))}
+Endereço de rede: {group_ip(ip_from_binary(infos['end_rede']))}
+Endereço de broadcast: {group_ip(ip_from_binary(infos['end_broadcast']))}
+Máscara de sub-rede: {group_ip(ip_from_binary(infos['mascara_sub_rede']))}
+Máscara de wildcard: {group_ip(ip_from_binary(infos['mascara_wildcard']))}
+Range: {group_ip(ip_from_binary(infos['range']['start']))} - {group_ip(ip_from_binary(infos['range']['end']))}
+Quantidade de hosts disponíveis: {infos['qnt_hosts']}
 ================================================================================
 ''')
     except ValueError as e:
